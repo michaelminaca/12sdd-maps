114a662926f49d87b9bf48447893851a9672e1d5
Michael Minaca <michaelminaca2004@gmail.com>
2022-04-20 20:28:19 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const workspaceContainer = document.querySelector('.workspace-container');
+const referenceNoteColumn = document.querySelector('.reference-note-column');
+const btnPlayPause = document.querySelector('.play-pause-btn');
+const btnRecording = document.querySelector('.record-btn');
+const btnSettings = document.querySelector('.settings-btn');
+const settingsMenu = document.querySelector('.settings-menu');
+
+const AppState = {
+  isPlaying: false,
+  isRecording: false,
+  isSettingsOpen: false,
+};
