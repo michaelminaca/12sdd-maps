da980af1b1a0498ec4a58ae84b7d8ad8b7fbf09d
Michael Minaca <michaelminaca2004@gmail.com>
2022-04-26 22:40:26 +1000

    Added playhead elements and functionality

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const workspaceContainer = document.querySelector('.workspace-container');
+const workspace = document.querySelector('.workspace');
 const referenceNoteColumn = document.querySelector('.reference-note-column');
 const btnPlayPause = document.querySelector('.play-pause-btn');
 const btnRecording = document.querySelector('.record-btn');
 const btnSettings = document.querySelector('.settings-btn');
 const settingsMenu = document.querySelector('.settings-menu');
+
+const playhead = document.querySelector('.playhead');
 
 const notes = [];
 const playbackQueue = [];
@@ -50,3 +52,11 @@ class Note {
 // function onMIDIMessage(event) {
 //   console.log('event');
 // }
+
+//Set
+workspace.addEventListener('click', (event) => {
+  playhead.style.transform = `translateX(${
+    (event.clientX - 40) * (100 / document.documentElement.clientWidth)
+  }vw)`;
+  console.log(playhead.style.transform);
+});
